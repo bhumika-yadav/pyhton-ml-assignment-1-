c0e4cdc37d5681b5d0f899dc632d10194326cff3
bhumika-yadav <147073211+bhumika-yadav@users.noreply.github.com>
2024-06-14 13:45:45 +0530

    Create ques16.py

---
+++ ques16.py
@@ -0,0 +1,13 @@
+#counts the frequency of each character
+a=input('enter a string')
+d={}
+
+for i in a:
+    if i in d:
+        d[i]=d[i]+1
+    else:
+        d[i]=1
+
+for i in d:
+    print(i,d[i])
+    print(end=" ")
